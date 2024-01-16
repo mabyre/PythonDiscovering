498614f697b0bbe3af783cc905a3abe8f7af55e1
mabyre <mabyre@sodevlog.com>
2024-01-16 17:28:19 +0100

    add a py to check version into modules

---
+++ versionModule.py
@@ -0,0 +1,22 @@
+""" Disply version for somme modules
+"""
+
+# import "dm-tree"
+# print("dm-tree: " + dm-tree.__version__)
+
+import pandas
+print("pandas: " + pandas.__version__)
+
+import sklearn
+print("sklearn: " + sklearn.__version__)
+
+
+# import tensorflow
+# print("tensorflow: " + tensorflow.__version__)
+
+import numpy
+print("numpy: " + numpy.__version__)
+
+
+import cmake
+print("cmake: " + cmake.__version__)
